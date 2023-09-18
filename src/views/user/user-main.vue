1a227bc4edbcb76dd84fc6a824ef283e5a8864a5
changxxxx <cguo8976@uni.sydney.edu.au>
2023-09-18 20:25:54 +1000

    [Subject]: Update user profile main page, added tabs [Author]: Chelsea

--- src/views/user/user-main.vue
+++ src/views/user/user-main.vue
@@ -1,11 +1,49 @@
 <template>
-    <div id="app-user-profile">
-        User Profile View
-    </div>
+    <el-container id="app-user-profile-container">
+        <el-tabs v-model="activeName" class="user-profile-tabs" @tab-click="handleClick" stretch>
+            <el-tab-pane label="Information" name="information">Information</el-tab-pane>
+            <el-tab-pane label="Follower/Following" name="follower">Follower/Following</el-tab-pane>
+            <el-tab-pane label="Posts" name="posts">Posts</el-tab-pane>
+            <el-tab-pane label="Portfolio/Photos" name="portfolio">
+                <PortfolioPhoto/>
+            </el-tab-pane>
+            <el-tab-pane label="Reviews" name="fifth">Reviews</el-tab-pane>
+        </el-tabs>
+    </el-container>
 </template>
 
-<script setup>
+<script>
+import { ref } from 'vue';
+import PortfolioPhoto from '@/pages/photo/profile-portfolio-photo.vue';
+
+export default {
+    components: {
+        PortfolioPhoto,
+    },
+    setup() {
+        const activeTab= ref('information');
+        const handleClick = (tab) => {
+            activeTab.value = tab.name;
+        };
+        return {
+            activeTab,
+            handleClick,
+        };
+
+    },
+};
 </script>
 
 <style scoped>
+#app-user-profile-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+}
+
+.user-profile-tabs {
+    width: 100%;
+}
 </style>
