55c0c2cd5613ae4292f05e5700f5dcdafb1f1e99
Dylan <drod2097@uni.sydney.edu.au>
2023-10-09 14:45:35 +1100

    added information to user profile

--- src/views/user/user-information/index.vue
+++ src/views/user/user-information/index.vue
@@ -1,27 +1,46 @@
 <template>
     <div class="app-user-information">
         <div class="app-title">My Profile</div>
+        <div class="profile-view-container">
+            <div class="profile-data"><img v-bind:src="profileData.avatarUrl == 'default' ? 'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg' : profileData.avatarUrl"/></div>
+            <div class="profile-label">First Name</div>
+            <div class="profile-data">{{profileData.firstName}}</div>
+            <div class="profile-label">Last Name</div>
+            <div class="profile-data">{{profileData.lastName}}</div>
+            <div class="profile-label">Alias</div>
+            <div class="profile-data">{{profileData.alias}}</div>
+            <div class="profile-label">User Name</div>
+            <div class="profile-data">{{profileData.userName}}</div>
+            <div class="profile-label">Email</div>
+            <div class="profile-data">{{profileData.email}}</div>
+            <div class="profile-label">Birthday</div>
+            <div class="profile-data">{{profileData.birthday}}</div>
+            <div class="profile-label">Description</div>
+            <div class="profile-data">{{profileData.description}}</div>
+            <div class="profile-label">Gender</div>
+            <div class="profile-data">{{profileData.gender}}</div>
+            <div class="profile-label">Phone Code</div>
+            <div class="profile-data">+{{profileData.phoneCode}}</div>
+            <div class="profile-label">Phone Number</div>
+            <div class="profile-data">{{profileData.phoneNumber}}</div>
+        </div>
     </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import postApi from '@/services/user-api';
+import { ElMessage } from 'element-plus';
 
 const loading = ref(false);
-const profileData = ref([{}]);
+const profileData = ref({});
 
-const fetchProfileData = async (isReload = false) => {
+const fetchProfileData = async () => {
     loading.value = true;
-    if (isReload){
-
-    }
     try{
-        const res = await postApi.getUserInformation(
-            filterProps.value
-        );
+        const res = await postApi.getUserProfile();
         if (res.code === 0){
-            profileData.value = res.data;
+            profileData.value = res.data[0];
         }else{
             ElMessage.error('Failed to get profile data');
         }
@@ -36,7 +55,7 @@ const fetchProfileData = async (isReload = false) => {
 }
 
 onMounted(() => {
-    fetchProfileData(true);
+    fetchProfileData();
 });
 
 </script>
@@ -53,5 +72,17 @@ onMounted(() => {
     font-size: 30px;
     text-align: left;
     font-weight: bold;
+}
+
+.profile-label {
+    font-size: 18px;
+    text-align: left;
+    font-weight: bold;
+}
+
+.profile-data {
+    font-size: 16px;
+    text-align: left;
+    margin-bottom: 10px;
 }
 </style>
