ec87caa04b59f5c89f863f97afd305c2b9d71ef4
zhaogongfan <gongfanzhao602@gmail.com>
2023-10-10 17:29:41 +1100

    Author：gongfan Date: Problem: Solution:create description component

---
+++ src/components/description/description.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div class="description">
+        <p class="description-detail"> {{ description.descriptionDetail }} </p>
+    </div>
+    
+</template>
+
+<script setup lang = 'ts'>
+import{ref} from 'vue'
+interface description{
+    descriptionDetail: string;
+}
+const description = ref<description>({
+    descriptionDetail: '这是一段描述',
+});
+</script>
+
+<style scoped>
+.description{
+    text-align: left;
+}
+</style>
