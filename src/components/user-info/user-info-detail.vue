6ab40ed96cb6646411bb643a3689323c5b955f31
zhaogongfan <gongfanzhao602@gmail.com>
2023-10-10 17:30:56 +1100

    Author：gongfan Date: Problem: Solution: create user-info-detail component

--- src/components/user-info/user-info-detail.vue
+++ src/components/user-info/user-info-detail.vue
@@ -1,26 +1,40 @@
 <template>
     <div class = "user-info-container">
         <div class = "user-info" >
-            <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" :size="70"></el-avatar>
-            <el-descriptions title="123456" >
-                <el-descriptions-item> @ftest </el-descriptions-item> 
-            </el-descriptions>
+            <el-avatar :src=userInfo.userAvatar :size="70"></el-avatar>
+            <div class="user-info-detail">
+                <div class="user-name">
+                    <el-link :underline="false">{{ userInfo.userName }}</el-link>
+                    <el-button type="text" icon="ChatDotSquare">message</el-button>
+                </div>
+                <div class="user-id">
+                    <el-text>@{{ userInfo.userId }}</el-text>
+                </div>
+            </div>
         </div>
         <el-button type="primary" icon="User">Follow</el-button>
     </div>
 </template>
 
-<script setup>
-import { User } from '@element-plus/icons-vue'
-// define props
-const props = defineProps({
-    // define props here
-    // userID
-    userId: {
-        type: String,
-        required: true
-    },
-})
+<script setup lang="ts">
+import { ref } from 'vue';
+interface userInfo{
+    userName : string;
+    userId : string;
+    userAvatar : string;
+}
+
+const userInfo = ref<userInfo>({
+    userName: '1234567',
+    userId: 'ftest1',
+    userAvatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+});
+
+
+
+
+
+
 </script>
 
 <style scoped>
@@ -34,9 +48,18 @@ const props = defineProps({
     justify-content: space-between;
 }
 
-.follow{
-    justify-content: flex-end
+.user-info-detail{
+    margin-left: 12px;
+    flex: 1;
+    overflow: auto;
 }
 
+.user-name{
+    display: flex;
+    align-items: center;
+}
+.user-id{
+    text-align: left;
+}
 
 </style>
