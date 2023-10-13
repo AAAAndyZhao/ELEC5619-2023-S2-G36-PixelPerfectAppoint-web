5387432ec0b815ce6cb9a6382c07a5ef1ba47eda
Yicheng <yzha5913@uni.sydney.edu.au>
2023-10-13 17:06:37 +1100

    [Subject]: photo components and views (not completed) [Author]: Yicheng Zhao

---
+++ src/components/photo/photo-card.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="app-photo-card">
+        <img :src="photo.thumbnail" :height="height" :width="width" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const emits = defineEmits(["click"]);
+const props = defineProps({
+    photo: {
+        type: Object,
+        required: true,
+    },
+    height: {
+        type: Number,
+        default: 300,
+    },
+    width: {
+        type: Number,
+        default: 300,
+    },
+});
+
+
+</script>
+
+<style scoped>
+
+</style>
