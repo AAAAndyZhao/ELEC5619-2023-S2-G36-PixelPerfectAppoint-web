300b836b3ad2c5d2982bef8f0ed9f205e61b0594
changxxxx <cguo8976@uni.sydney.edu.au>
2023-10-23 00:13:47 +1100

    [Subjects]: 1. Add protfolio and post view 2. Complete hide/show portfolio. [Author]: Chelsea Guo

---
+++ src/views/user/user-portfolio-photo/user-portfolio-list.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="app-user-portfolios-list">
+        <PortfolioCard v-for="portfolio in data" :key="portfolio.id" :portfolio="portfolio"
+            @customClick="handleCustomClick"></PortfolioCard>
+
+
+    </div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { Search } from '@icon-park/vue-next';
+import userApi from '@/services/user-api';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import PortfolioCard from '@/components/photo/portfolio-card.vue';
+
+const emits = defineEmits(['customClickFromB']);
+const props = defineProps({
+    data: {
+        type: Array,
+        required: false,
+        default: () => []
+    }
+})
+
+const handleCustomClick = (portfolioData) => {
+    emits('customClickFromB', portfolioData);
+}
+
+</script>
+
+<style scoped>
+.app-user-portfolios-list {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    gap: 10px;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-content: flex-start;
+}
+</style>
