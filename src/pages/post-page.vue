c8c1525fb1b92232a655bb2c61872cc9768b0601
zhaogongfan <gongfanzhao602@gmail.com>
2023-10-23 09:11:57 +1100

    Author：gongfan Date: Problem: Solution:post detail show

--- src/pages/post-page.vue
+++ src/pages/post-page.vue
@@ -8,10 +8,10 @@
                 <el-button type="info" class="back" @click="backTo" icon="DArrowLeft" round></el-button>
             </div>
             <el-card class="app-main-post-title">
-                <router-view name="title" />
+                <router-view name="title" :postInfo="postInfo" />
             </el-card>
             <el-card class="app-main-post-photo">
-                <router-view name="content" />
+                <router-view name="content" :postInfo="postInfo"/>
             </el-card>
             <el-card>
                 <like theme="filled" size="30" :fill="isLiked ? '#ff0000' : '#333'" @click="toggleLike" />
@@ -19,11 +19,11 @@
 
             <el-card class="app-main-user-info">
                 <div>
-                    <router-view name="info" />
+                    <router-view name="info" :user="userInfo" />
                 </div>
             </el-card>
             <el-card class="app-main-post-description">
-                <router-view name="description" />
+                <router-view name="description" :postInfo="postInfo"/>
             </el-card>
             <el-card>
                 <router-view name="comment" />
@@ -33,16 +33,41 @@
     </el-contrainer>
 </template>
 
-<script setup lang="ts">
+<script setup>
 
 import { ElCard } from 'element-plus';
 import router from '../router'
 import { Like } from '@icon-park/vue-next'
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import userApi from '../services/user-api';
+import postApi from '../services/post-api';
 
-//点击返回按钮返回上一页
+const userInfo = ref([])
+const postInfo = ref([])
 const isLiked = ref(false);
+const userId = localStorage.getItem('userId');
 
+
+const getPostDetail = async () => {
+    let path = window.location.pathname;
+    let parts = path.split('/');
+    let postId = parts[parts.length - 1];
+    try {
+        const res = await postApi.getPostDetail(postId);
+        
+        if (res.code === 0) {
+            userInfo.value=res.data[0].author;
+            postInfo.value = res.data[0];
+            console.log('userInfo',userInfo.value)
+            console.log('postInfo',res.data[0].photos)
+        }
+
+    }
+    catch (e) {
+        console.log(e);
+    }
+
+}
 const toggleLike = () => {
     isLiked.value = !isLiked.value;
 };
@@ -50,6 +75,11 @@ const toggleLike = () => {
 const backTo = () => {
     router.back();
 };
+
+onMounted(() => {
+    
+    getPostDetail();
+});
 
 
 
@@ -119,4 +149,5 @@ const backTo = () => {
 .app-main-post-description {
     border: none !important;
     box-shadow: none !important;
-}</style>
+}
+</style>
