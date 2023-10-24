63574d20a6d07b1d30026433f96b14103216f2d4
Weikun <weli4073@uni.sydney.edu.au>
2023-10-24 20:15:39 +1100

    [Subject]: refactor the portfolio create and display, adjust the appointment [Author]: Lucas

--- src/components/photo/photo-image.vue
+++ src/components/photo/photo-image.vue
@@ -1,24 +1,25 @@
 <template>
-    <el-image :src="imageSrc" :alt="alt" :style="style" :fit="fit" :hide-on-click-modal="hideOnClickModal"
-        :loading="loading" :lazy="lazy" :scroll-container="scrollContainer" :referrerpolicy="referrerpolicy"
-        :preview-src-list="previewSrcList" :z-index="zIndex" :initial-index="initialIndex"
-        :close-on-press-escape="closeOnPressEscape" :preview-teleported="previewTeleported" :infinite="infinite"
-        :zoom-rate="zoomRate" :min-scale="minScale" :max-scale="maxScale"
-        v-loading="imgLoading">
-        <!-- pass templates to el-image -->
-        <template #error>
-            <slot name="error">
-            </slot>
-        </template>
-        <template #placeholder>
-            <slot name="placeholder">
-            </slot>
-        </template>
-        <template #viewer>
-            <slot name="viewer">
-            </slot>
-        </template>
-    </el-image>
+    <div v-loading="imgLoading">
+        <el-image :src="imageSrc" :alt="alt" :style="style" :fit="fit" :hide-on-click-modal="hideOnClickModal"
+            :loading="loading" :lazy="lazy" :scroll-container="scrollContainer" :referrerpolicy="referrerpolicy"
+            :preview-src-list="previewSrcList" :z-index="zIndex" :initial-index="initialIndex"
+            :close-on-press-escape="closeOnPressEscape" :preview-teleported="previewTeleported" :infinite="infinite"
+            :zoom-rate="zoomRate" :min-scale="minScale" :max-scale="maxScale" v-show="showThePhoto">
+            <!-- pass templates to el-image -->
+            <template #error>
+                <slot name="error">
+                </slot>
+            </template>
+            <template #placeholder>
+                <slot name="placeholder">
+                </slot>
+            </template>
+            <template #viewer>
+                <slot name="viewer">
+                </slot>
+            </template>
+        </el-image>
+    </div>
 </template>
 
 <script setup>
@@ -106,6 +107,7 @@ const emits = defineEmits([
     'show'
 ]);
 // emit all el-images events
+const showThePhoto = ref(false);
 const imgLoading = ref(false);
 const imageSrc = ref('');
 const fetchImage = async () => {
@@ -133,10 +135,13 @@ const fetchImage = async () => {
         }
         const blob = await res.data;
         imageSrc.value = URL.createObjectURL(blob);
+        setTimeout(() => {
+            showThePhoto.value = true;
+        }, 1000);
     } catch (error) {
         console.error(error);
         return '';
-    }finally {
+    } finally {
         imgLoading.value = false;
     }
 }
