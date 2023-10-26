ba10fcf1a9b8bd627f900e0a8003e3b967179d75
Weikun <weli4073@uni.sydney.edu.au>
2023-10-26 13:01:01 +1100

    [Subject]: Fix the photo-viewer has drag offset when zoom in/out. Correct the min-height of the portfolio page. [Author]: Lucas

--- src/views/user/user-portfolio-photo/user-portfolio-list.vue
+++ src/views/user/user-portfolio-photo/user-portfolio-list.vue
@@ -39,6 +39,6 @@ const handleDeletePortfolio = (portfolioData) => {
     flex-wrap: wrap;
     justify-content: flex-start;
     align-content: flex-start;
-    min-height: 600px;
+    min-height: calc(100vh - 300px);
 }
 </style>
