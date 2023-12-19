72a6f60f6b54cba488b7b6eebb62d9b2909910c5
AAAAndyZhao <andyzhaoccc@gmail.com>
2023-12-19 15:21:11 +0800

    add desensitized public.config.js

---
+++ public/public.config.js
@@ -0,0 +1,6 @@
+// this is a public js file, you can define some static menus here
+$PUBLIC = {
+    SERVER_HOST: "http://localhost:8080/api",
+    GOOGLE_MAP_API_KEY: "Your Google Map API key",
+    INF_INT: 2147483647,
+}
